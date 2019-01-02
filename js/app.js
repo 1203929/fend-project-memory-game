e497a796488929fe34b33cb19a18ecb0a9d837a7
Masnoon Junaid <masnoon20@gmail.com>
2019-01-02 22:25:17 +0530

    create DOM element with JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 /*
  * Create a list that holds all of your cards
  */
- let cardList = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-anchor','fa-leaf','fa-bicycle',
-                'fa-diamond','fa-bomb','fa-leaf','fa-bomb','fa-bolt','fa-bicycle',
-                'fa-paper-plane-o','fa-cube']
+ const cardList = ['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt',
+                   'fa-cube','fa-anchor','fa-leaf','fa-bicycle',
+                'fa-diamond','fa-bomb','fa-leaf','fa-bomb','fa-bolt',
+                'fa-bicycle','fa-paper-plane-o','fa-cube'];
 
+const cardContainer  = document.querySelector('.deck');
+
+for (let i = 0;i<cardList.length; i++){
+  const card = document.createElement('li');
+  card.classList.add("card");
+  card.innerHTML = `<i class="${cardList[i]}"></i>`;
+  cardContainer.appendChild(card);
+}
+
+
+
+
+
+
+
+/*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
@@ -28,106 +45,6 @@ function shuffle(array) {
 
 
 //shuffling card upon page load or page refresh
-document.body.onload = launchGame();
-//adding eventListener to launchGame function, by clicking restart button
-restartButton.addEventListener('click',launchGame);
-function launchGame(){
-  patti = shuffle(patti);
-  for (let i = 0; i<patt.length; i++){
-    patti.forEach(i => deck.appendChild(i));
-    patti[i].classList.remove('show','open','Matching','disabled');
-  }
-  showCards = [];
-  //resseting number of Moves
-  moves = 0;
-  counter.innerHTML = moves;
-  //resetting rating
-  for (let i =0; i<twinkle.length; i++){
-    twinkle[i].style.color = '#00a1ff';
-
-    twinkle[i].style.display = 'inline'
-  }
-  //to ressets timer
-  let second = 0;
-  let minute = 0;
-  let hour = 0;
-  let  timer = document.querySelector('.timer');
-  timer.innerHTML = '0 mins 0 secs';
-  clearInterval(interval);
-};
-
-//applying method to open and show cards
-let showCards = function () {
-  this.classList.toggle('open');
-  this.clsssList.toggle('show');
-  this.classList.toggle('disabled');
-};
-
-//adding visible symbol's card into an array named openCards and checking weather two cards match or
- function visibleCard(){
-   openCards.push(this);
-   let len = openCards.length;
-
-   if(len == 1 || Moves == 0) {
-     second = 0;
-     minute = 0;
-     hour = 0;
-     startTimer();
-   }
-   if(len = 1 && moves == 0) {
-     second = 0;
-     minute = 0;
-     hour = 0;
-     startTimer();
-   } else if(len === 2) {
-     moveCounter();
-     if (openCards[0].type ===openCards[1].type) {
-       matchCon();
-// matchCon function run when two cards matched
-}else {
-  notMatching();// if two cards does't match
-     }
-   }
- }
-
-//functionality when cards match
-// + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-function matchCon() {
-  openCards[0].classList.add('Matching', 'disabled');
-  openCards[1].classList.add('Matching', 'disabled');
-  openCards[0].classList.remove('show', 'open');
-  openCards[1].classList.remove('show','open');
-  openCards = [];
-};
-
-// when cards don't match
-// if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-function notMatching(){
-  openCards[0].classList.add('not_matching');
-  openCards[1].classList.add('not_matching');
-  disable();
-  setTimeout(function(){
-    openCards[0].clssList.remove('show', 'open', 'not_matching');
-    openCards[1]classList.remove('show', 'open','not_matching');
-    enable();
-    openCards = [];
-  },550);
-}
-
-// disabling card for a while two cards are flipped
-function disable() {
-  cards.forEach(card =>card.classList.add('disabled'));
-}
-
-//enabling cards flipping and disabling matched cards
-function enable(){
-  array.prototype.filter.call(patti,function(card){
-    card.classList.remove('disabled');
-    for let(let i = 0; i<matchingCard.length; i++){
-      matchingCard[i].classList.add('disabled');
-    }
-  });
-}
 
 //setting game timer function
 function setTimer() {
@@ -148,24 +65,6 @@ function setTimer() {
 // *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 //setting star rating based on number of moves to flipped the cardd
 
-function moveCounter() {
-  moves++;
-  counter.innerHTML = moves;
-  if (moves >8 && moves < 14) {
-    for (i =0; i < 3; i++) {
-      if (i > 1) {
-        twinkle[i].style.display = 'none';
-      }
-    }
-  }
-  else if (moves > 15) {
-    for (i = 0; i < 3; i++) {
-      if (i >0) {
-        twinkle[i].style.display = 'none';
-      }
-    }
-  }
-}
 
 // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 //Adding a congratulations modal after all cards matched successfully
